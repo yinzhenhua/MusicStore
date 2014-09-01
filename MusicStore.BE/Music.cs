1bcfc5c2b17e9d2ad1045299d3f3cb3cd36766f8
xinding2 <xinding2@iflytek.com>
2014-09-01 22:34:17 +0800

    add Music class by xinding2

---
+++ MusicStore.BE/Music.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicStore.BE
+{
+    public class Music
+    {
+        private int id;
+        private string name;
+    }
+}
